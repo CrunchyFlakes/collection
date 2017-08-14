de87df7df319fe18de3d7d18d57b9c5890791b29
CrunchyFlakes <m.toepperwien@googlemail.com>
2017-08-14 16:23:48 +0200

    update

--- football_calendar.py
+++ football_calendar.py
@@ -25,6 +25,6 @@ for row in table:
                     datetimes.append(datetime(int(date[2]) + 2000, int(date[1]), int(date[0]), int(time[0]), int(time[1])))
 
 
-print("{:0>2}.{:0>2}".format(datetime.now().day, datetime.now().month))
+print(datetime.now().strftime("%A")[:3] + " " +  "{:0>2}.{:0>2}.{:0>4}".format(datetime.now().day, datetime.now().month, datetime.now().year) + " (today)")
 for date in datetimes:
     print(date.strftime("%A")[:3] + " " + "{:0>2}.{:0>2}.{:0>4} {:0>2}:{:0>2}".format(date.day, date.month, date.year, date.hour, date.minute))
