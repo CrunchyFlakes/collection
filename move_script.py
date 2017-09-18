520ffd0d5b23e8062a5d18b546632b0b8e7f7548
CrunchyFlakes <m.toepperwien@googlemail.com>
2017-09-18 20:19:17 +0200

    update

--- move_script.py
+++ move_script.py
@@ -21,9 +21,10 @@ for object in directory_listed:
                 os.rename(object_path, new_object_path)
                 print(object_path + " moved to: " + season_folder_path + "/" + object_path.split("/")[-1])
                 if " " in object_path:
-                    new_object_path_split = new_object_path.split("/")
-                    new_object_path_split[-1] = new_object_path_split[-1].replace(" ", "_").lower()
-                    new_object_path = "/".join(new_object_path_split)
+                    object_path = new_object_path
+                    object_path_split = object_path.split("/")
+                    object_path_split[-1] = object_path_split[-1].replace(" ", "_").lower()
+                    new_object_path = "/".join(object_path_split)
                     os.rename(object_path, new_object_path)
 
 print("finished")
