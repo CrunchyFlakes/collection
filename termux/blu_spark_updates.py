42bc49049823689004168bb3df439e0019da79eb
CrunchyFlakes <m.toepperwien@googlemail.com>
2018-06-15 00:44:18 +0200

    added termux blu_spark update script

--- termux/blu_spark_updates.py
+++ termux/blu_spark_updates.py
@@ -8,22 +8,25 @@ import subprocess
 
 title = lxml.html.parse(urlopen("https://forum.xda-developers.com/oneplus-6/development/kernel-t3800965")).findtext(".//title")
 
-file = open("blu_spark_version.txt", "r+")
+file = open("blu_spark_version.txt", "r")
 
 version_installed = file.readline()
+file.close()
 
 version_index = title.find("blu_spark r")
 string_length = 13
 version = title[version_index:version_index + 13]
 
 if version_installed == version:
-    print("newest version installed")
+    print("newest version installed: " + version_installed)
 else:
-    print(version)
-    print(version_installed)
-    print(title)
-    notification_command = "termux-notification -t \"blu_spark Update\" -c \"New version" + version + "\" --action \"termux-open-url https://forum.xda-developers.com/devdb/project/?id=27466#downloads\""
-    notification = subprocess.Popen(notification_command, shell=True)
+    print("new version: " + version)
+    print("current version: " + version_installed)
+    #notification_command = "termux-notification -t \"blu_spark Update\" -c \"New version" + version + "\" --action \"termux-open-url https://forum.xda-developers.com/devdb/project/?id=27466#downloads\""
+    #notification = subprocess.Popen(notification_command, shell=True)
+    file = open("blu_spark_version.txt", "w")
+    file.writelines(version)
+    file.close()
 
 
 
