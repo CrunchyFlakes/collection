7c52b65fc90687350b1ff0e3da2362ebc633104c
CrunchyFlakes <m.toepperwien@googlemail.com>
2018-06-15 00:45:01 +0200

    added termux blu_spark update script

--- termux/blu_spark_updates.py
+++ termux/blu_spark_updates.py
@@ -22,8 +22,8 @@ if version_installed == version:
 else:
     print("new version: " + version)
     print("current version: " + version_installed)
-    #notification_command = "termux-notification -t \"blu_spark Update\" -c \"New version" + version + "\" --action \"termux-open-url https://forum.xda-developers.com/devdb/project/?id=27466#downloads\""
-    #notification = subprocess.Popen(notification_command, shell=True)
+    notification_command = "termux-notification -t \"blu_spark Update\" -c \"New version" + version + "\" --action \"termux-open-url https://forum.xda-developers.com/devdb/project/?id=27466#downloads\""
+    notification = subprocess.Popen(notification_command, shell=True)
     file = open("blu_spark_version.txt", "w")
     file.writelines(version)
     file.close()
